839bf75903fb5cd0fb3ebd3d9f7d0590ef548d5a
Paulo Fernando <74271792+Ppaulofernando@users.noreply.github.com>
2022-03-24 12:42:22 -0300

    Update NumerosPrimos.java
    Trocando os nomes da variáveis para ficar mais claro .

--- NumerosPrimos.java
+++ NumerosPrimos.java
@@ -6,36 +6,29 @@ import java.util.Scanner;
 public class NumerosPrimos {
 
 	public static void main(String[] args) {
-		// Número primo: divísivel é só por 1 e ele mesmo.
+		// NÃºmero primo: divÃ­sivel Ã© sÃ³ por 1 e ele mesmo.
 		
 		Scanner leitor = new Scanner(System.in);
-		int numeroPrimo;
+		int numeroDigitado;
 		int contadorDivisores = 0;
-		
-		System.out.print("Digite um número: ");
-		numeroPrimo = leitor.nextInt();
-		
-		
-		for(int i = 1; i <= numeroPrimo; i++) {
-			if(numeroPrimo % i == 0) {
-				contadorDivisores++;		
+
+		System.out.print("Digite um nÃºmero: ");
+		numeroDigitado = leitor.nextInt();
+
+		for (int i = 1; i <= numeroDigitado; i++) {
+			if (numeroDigitado % i == 0) {
+				contadorDivisores++;
 			}
-		}	
-			
-			if (contadorDivisores == 2) {
-				System.out.println("Número: " + numeroPrimo + " é primo.");
-			} else {
-				System.out.println("Número: " + numeroPrimo + " não é primo.");
-			} 
-			System.out.println("Número de divisores: " + contadorDivisores);
-			
-			
-		leitor.close();	
-			
 		}
-		
-	
 
-	
+		if (contadorDivisores == 2) {
+			System.out.println("NÃºmero: " + numeroDigitado + " Ã© primo.");
+		} else {
+			System.out.println("NÃºmero: " + numeroDigitado + " nÃ£o Ã© primo.");
+		}
+		System.out.println("NÃºmero de divisores: " + contadorDivisores);
 
+		leitor.close();
+	}
 }
+
